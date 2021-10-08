760697278c3ad59276776b4a9818df674c22f737
huawei laptop <huawei@laptop.com>
2021-10-08 13:52:56 +0800

    remove -Dmaven.main.skip

--- mall-devops/delete_wechat_user.sql
+++ mall-devops/delete_wechat_user.sql
@@ -1,3 +1,6 @@
 
--- SELECT id,uid,login_name from `alliance`.t_user where uid='U011910212334540002'
+SELECT id,uid,login_name,phone from `alliance`.t_user where uid='U011910212334540002'
+SELECT id,uid,login_name,phone from `alliance`.t_user where phone='13660123456'
+
 DELETE FROM `alliance`.t_user WHERE uid='U011910212334540002'
+DELETE FROM `alliance`.t_user WHERE phone='13660878929'
